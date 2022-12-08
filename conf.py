0057c28c1b45e480cb5945f4f27b40887d8a4792
Jung Hyuk Lee <ljh0412@gist.ac.kr>
2022-12-08 14:39:39 +0900

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,28 @@
+import torch
+from transformers import TrainingArguments
+
+train = False
+evaluate = True
+feat_ext = False
+infer = False
+
+inputs = 'path'
+outputs = 'asd'
+batch_size = 128
+
+db_type = 'db_type'
+
+save_path = 'save_path'
+wav_path = f'{save_path}/{db_type}'
+df_names = [f'train_{db_type}.csv', f'eval_{db_type}.csv', f'infer_{db_type}.csv']
+
+model_name = 'facebook/wav2vec2-base-960h'
+fix_feat_ext = False 
+condition = 'fix-fe' if fix_feat_ext else 'tune-fe'
+exp_dir = './exp/'
+
+pooling_mode = 'mean'
+is_regression = False
+sampling_rate = 16000
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
